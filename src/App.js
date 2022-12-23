8a61de1c7db673f7df7194b4a9ccf043af268539
Likhitha <likitha.9678@gmail.com>
2022-12-23 22:45:31 +0530

    addd

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <apple-pay-button buttonstyle="white-outline" type="pay" locale="en"></apple-pay-button>
+<h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
