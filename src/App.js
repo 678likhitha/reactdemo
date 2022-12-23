9424025e090fc598d782bed5d03b1bd2f9026858
Likhitha <likitha.9678@gmail.com>
2022-12-23 22:49:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <apple-pay-button buttonstyle="white-outline" type="pay" locale="en"></apple-pay-button>
+      <apple-pay-button buttonstyle="white-outline" type="pay" locale="en">PAybuttion</apple-pay-button>
 <h1>Hello</h1>
     </div>
   );
